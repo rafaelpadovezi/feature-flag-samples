4d3715ec865040abff5121db81ae6c14478b06ba
Rafael Miranda <rafaelpadovezi@gmail.com>
2023-05-02 10:43:04 -0300

    feat(launchdarkly): load ff from file

--- aspnet-launchdarkly/src/Program.cs
+++ aspnet-launchdarkly/src/Program.cs
@@ -1,5 +1,6 @@
 using Flaggy;
 using LaunchDarkly.Sdk.Server;
+using LaunchDarkly.Sdk.Server.Integrations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -52,13 +53,27 @@ app.Run();
 
 LdClient AddLaunchDarklyClient(WebApplicationBuilder webApplicationBuilder)
 {
+    var online = webApplicationBuilder.Configuration.GetValue<bool>("LaunchDarkly:Online");
     var launchDarklySdkKey = webApplicationBuilder.Configuration.GetValue<string>("LaunchDarkly:SdkKey");
-    // The initial payload contains all the targeting rules for the environment associated with the provided SDK key.
-    // If you target a large number of individual contexts, have a very large number of flags, or have large numbers
-    // of extremely complicated rules, then this can increase the initial payload size. Configure any relevant timeout
-    // options to anticipate a large initial load. This helps prevent the SDK from timing out.
-    // https://docs.launchdarkly.com/sdk/concepts/getting-started#plan-for-a-large-initial-payload-from-the-streaming-endpoint
-    var ldConfig = Configuration.Default(launchDarklySdkKey);
+    Configuration ldConfig;
+
+    if (online)
+        // The initial payload contains all the targeting rules for the environment associated with the provided SDK key.
+        // If you target a large number of individual contexts, have a very large number of flags, or have large numbers
+        // of extremely complicated rules, then this can increase the initial payload size. Configure any relevant timeout
+        // options to anticipate a large initial load. This helps prevent the SDK from timing out.
+        // https://docs.launchdarkly.com/sdk/concepts/getting-started#plan-for-a-large-initial-payload-from-the-streaming-endpoint
+        ldConfig = Configuration.Default(launchDarklySdkKey);
+    else
+        ldConfig = Configuration.Builder("TEST-SDK-KEY")
+            .DataSource(
+                FileData.DataSource()
+                    .FilePaths("../ld-flagdata.json")
+                    .AutoUpdate(true)
+            )
+            .Events(Components.NoEvents)
+            .Build();
+
     var ldClient = new LdClient(ldConfig);
     // Your SDK should only initialize once statically, and that instance should be the exclusive instance you use
     // for a given LaunchDarkly project. When you create multiple SDK instances, previous instances may leak into
